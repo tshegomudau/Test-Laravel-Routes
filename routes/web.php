86c9c5882a190ddc097769a55ebfdf7a457e97ab
tshegomudau <tshegomudau@gmail.com>
2022-05-17 20:12:58 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,34 +15,34 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
-
+Route::get('/', ['as' => 'home',  'uses' => 'HomeController@index']);
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
-
+Route::get('/user/[name]', ['as' => 'show',  'uses' => 'UserController@show']);
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::get('/about', function () {return view('pages/about');});
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-
+Route::redirect('/log-in', '/login');
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
+Route::group(['middleware' => 'auth:sanctum'], function() {
 
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
     // Add another group for routes with prefix "app"
     // Put one Route Group code line here below
-
-        // Tasks inside that /app group:
+Route::prefix('app')->group(function () {
 
 
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
@@ -53,9 +53,10 @@ use Illuminate\Support\Facades\Route;
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
+    Route::resource('tasks', 'TaskController');
 
     // End of the /app Route Group
-
+});
 
     // Task 9: /admin group within a group
     // Add a group for routes with URL prefix "admin"
@@ -77,6 +78,7 @@ use Illuminate\Support\Facades\Route;
     // End of the /admin Route Group
 
 // End of the main Authenticated Route Group
+});
 
 // One more task is in routes/api.php
 
